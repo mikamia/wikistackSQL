04f37443490b203b6eae95af88944127aae35445
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-21 12:39:13 -0400

    fixed bug

--- models/index.js
+++ models/index.js
@@ -6,27 +6,29 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 
 
 var Page = db.define('page', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    urlTitle: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    status: {
+      type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
   },
-  urlTitle: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  status: {
-    type: Sequelize.ENUM('open', 'closed')
-  },
-  date: {
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.NOW
-  },
-  getterMethods: {
-    route: function() {return '/wiki/' + this.urlTitle; }
+  {
+    getterMethods: {
+    route: function () {return '/wiki/' + this.urlTitle; }
   }
 });
 
