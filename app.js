a841000db78ee83615f85e1627b405db5a84f0f6
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-21 17:29:07 -0400

    what users

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var models = require('./models');
 var wikiRouter = require('./routes/wiki');
+var userRouter = require('./routes/users');
 
 // logging middleware
 app.use(morgan('dev'));
@@ -42,3 +43,4 @@ models.User.sync({})
 .catch(console.error);
 
 app.use('/wiki', wikiRouter);
+app.use('/users', userRouter);
