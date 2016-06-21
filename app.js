a88b73db54bdf01e1160a015897a53d63cd48df8
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-21 11:50:21 -0400

    getting started done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var morgan = require('morgan');
 var swig = require('swig');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 // logging middleware
 app.use(morgan('dev'));
@@ -13,3 +14,23 @@ app.use(express.static(path.join(__dirname, '/public')));
 // body parsing middleware
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
+
+
+//Incorporates swig into its rendering.
+// point res.render to the proper directory
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+app.get('/', function(req,res){
+  console.log('cool');
+});
+
+app.listen(3000, function() {
+  console.log('listening to port 3000...');
+})
