3c6c6ae88d942b0936c079a96504ea546db7a1a7
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-21 12:08:49 -0400

    added page definition

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 var Sequelize = require('sequelize');
 
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+var Page = db.define('page', {
+  title: Sequelize.STRING,
+  urlTitle: Sequelize.STRING,
+  content: Sequelize.STRING,
+  date: Sequelize.DATE,
+  status: Sequelize.BOOLEAN
+});
