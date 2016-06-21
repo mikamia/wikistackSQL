c9dff286596202e97477687ef36c12eae3f7b073
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-21 11:40:18 -0400

    setup basic stuff

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var swig = require('swig');
+var bodyParser = require('body-parser');
 
 // logging middleware
 app.use(morgan('dev'));
+//Serves up static files from some kind of public folder.
+app.use(express.static(path.join(__dirname, '/public')));
+//Parses the request body.
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
