3491e4b928c16fa0878ef76fdd1091f9b3e203c4
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-21 12:21:49 -0400

    psql logging

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var morgan = require('morgan');
 var swig = require('swig');
 var bodyParser = require('body-parser');
 var path = require('path');
+var models = require('./models');
 
 // logging middleware
 app.use(morgan('dev'));
@@ -27,10 +28,19 @@ app.engine('html', swig.renderFile);
 // turn of swig's caching
 swig.setDefaults({cache: false});
 
-app.get('/', function(req,res){
+app.get('/', function (req, res){
   console.log('cool');
 });
 
-app.listen(3000, function() {
-  console.log('listening to port 3000...');
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({});
 })
+.then(function () {
+  app.listen(3000, function () {
+    console.log('listening to port 3000...');
+  });
+})
+.catch(console.error);
+
+
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging:false
+});
 
 
 var Page = db.define('page', {
