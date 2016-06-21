cb8f5bd4266349f13b66d18b02a024b2f515f057
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-21 12:24:37 -0400

    added checks to users and page

--- models/index.js
+++ models/index.js
@@ -7,28 +7,36 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 
 var Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    isUrl: true
   },
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false
   },
   status: {
     type: Sequelize.ENUM('open', 'closed')
   },
   date: {
-    type: Sequelize.DATE
+    type: Sequelize.DATE,
+    isDate: true
   }
 });
 
 var User = db.define('user', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull:false,
+    isEmail: true
   }
   });
 
