43731878598f1f462d71bd9f4d9e924816cb5c01
Ayako Mikami <ayakomikami11@gmail.com>
2016-06-21 12:53:57 -0400

    res.json

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,15 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('got to GET /wiki/');
+  res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
-  res.send('got to POST /wiki/');
+  res.json(req.body);
 });
 
 router.get('/add', function(req, res, next) {
-  res.send('got to GET /wiki/add');
+  res.render('addpage');
 });
 
 module.exports = router;
