907d45d82378c30ea123334a693b4bf07b663859
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-21 12:08:34 -0400

    user model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,8 @@
 var Sequelize = require('sequelize');
 
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+var User = db.define('user', {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
+  });
